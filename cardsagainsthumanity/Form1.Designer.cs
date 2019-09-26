bd95f0cd91aa387ae3451a31cb660b04df38c4de
haloreach252 <haloreach252@gmail.com>
2019-09-26 12:09:30 -0600

    Started work on cards and got reading started

--- cardsagainsthumanity/Form1.Designer.cs
+++ cardsagainsthumanity/Form1.Designer.cs
@@ -23,13 +23,54 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.titleLabel = new System.Windows.Forms.Label();
+			this.tempButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// titleLabel
+			// 
+			this.titleLabel.AutoSize = true;
+			this.titleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.titleLabel.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold);
+			this.titleLabel.Location = new System.Drawing.Point(310, 15);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(580, 58);
+			this.titleLabel.TabIndex = 0;
+			this.titleLabel.Text = "Cards Against Humanity";
+			// 
+			// tempButton
+			// 
+			this.tempButton.BackColor = System.Drawing.Color.Black;
+			this.tempButton.Location = new System.Drawing.Point(234, 239);
+			this.tempButton.Name = "tempButton";
+			this.tempButton.Size = new System.Drawing.Size(75, 23);
+			this.tempButton.TabIndex = 1;
+			this.tempButton.Text = "button1";
+			this.tempButton.UseVisualStyleBackColor = false;
+			this.tempButton.Click += new System.EventHandler(this.tempButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(1184, 661);
+			this.Controls.Add(this.tempButton);
+			this.Controls.Add(this.titleLabel);
+			this.ForeColor = System.Drawing.Color.White;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.Text = "Cards Against Humanity";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label titleLabel;
+		private System.Windows.Forms.Button tempButton;
 	}
 }
 
--- cardsagainsthumanity/Form1.cs
+++ cardsagainsthumanity/Form1.cs
@@ -7,11 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Diagnostics;
 
 namespace cardsagainsthumanity {
 	public partial class Form1 : Form {
+
+		private List<string> whiteCards;
+		private List<string> blackCards;
+
+		string path = AppDomain.CurrentDomain.BaseDirectory + @"\cards.xml";
+
 		public Form1() {
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			//if (Debugger.IsAttached && Environment.MachineName == "") path = "";
+
+			// Centers the title label at the top middle regardless of display size
+			titleLabel.Top = 15;
+			titleLabel.Left = (ClientSize.Width - titleLabel.Width) / 2;
+
+			//whiteCards = GetCards("white");
+			//blackCards = GetCards("black");
+		}
+
+		private List<string> GetCards(string color) {
+			List<string> data = new List<string>();
+			//XmlReader reader = XmlReader.Create();
+			return data;
+		}
+
+		private void tempButton_Click(object sender, EventArgs e) {
+
 		}
 	}
 }
